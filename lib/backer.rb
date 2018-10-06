8d25058568dbcd098f79117b1f2f3804a5f980a2
Gina Fedder <ginafedder@yahoo.com>
2018-10-06 19:13:58 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -10,8 +10,8 @@ class Backer
     @backed_projects
   end 
   
-  def back_project(name)
-    @backed_projects << name
+  def back_project(project)
+    @backed_projects << project
   end 
   
 end 
